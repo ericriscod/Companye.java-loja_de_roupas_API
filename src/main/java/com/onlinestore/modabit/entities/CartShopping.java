2249c5a03febf61f9a2b7f630a4fd0a5719c9b09
Ericris Rossato <rossato.ep@gmail.com>
2023-02-12 14:15:20 -0300

    Updation the entity CartShopping

--- src/main/java/com/onlinestore/modabit/entities/CartShopping.java
+++ src/main/java/com/onlinestore/modabit/entities/CartShopping.java
@@ -1,36 +1,20 @@
 package com.onlinestore.modabit.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 import java.util.Set;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "tb_cartshopping")
-public class CartShopping implements Serializable{
+public class CartShopping implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	
-	private Long id;
-	
-	@OneToMany
+
 	private Set<Product> products;
-	
+
 	private Double price;
 	private Integer quantity;
-	
+
 	public CartShopping() {
 	}
 
 	public CartShopping(Set<Product> products, Double price, Integer quantity) {
-		super();
 		this.products = products;
 		this.price = price;
 		this.quantity = quantity;
@@ -59,28 +43,5 @@ public class CartShopping implements Serializable{
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CartShopping other = (CartShopping) obj;
-		return Objects.equals(id, other.id);
-	}
-	
-	
 
 }
